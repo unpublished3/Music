38013fe0f347e81d678bf92023a505269f1d7463
unpublished3 <chapagainsakshyam@gmail.com>
2024-03-02 08:01:18 +0545

    Align all the UI elements for player

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -7,46 +7,89 @@ class PlayerUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 150),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Music Image
+          Thumbnail(),
+
+          Name(),
+
+          StatusBar(),
+
+          MediaControls()
+        ],
+      ),
+    );
+  }
+}
+
+class MediaControls extends StatelessWidget {
+  const MediaControls({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Icon(Icons.skip_previous),
+        Icon(Icons.pause_circle_filled_rounded),
+        Icon(Icons.skip_next)
+      ],
+    );
+  }
+}
+
+class StatusBar extends StatelessWidget {
+  const StatusBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        // Music Image
-        Flexible(
-          fit: FlexFit.tight,
-          flex: 3,
-          child: FractionallySizedBox(
-            heightFactor: 0.3,
-            widthFactor: 0.6,
-            child: Container(
-              color: Colors.red,
-            ),
-          ),
+        LinearProgressIndicator(
+          value: 0.1,
         ),
-
-        Column(
-          children: [Text("Name"), Text("Artist")],
-        ),
-
-        Column(
-          children: [
-            LinearProgressIndicator(
-              value: 0.1,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [Text("Current Time"), Text("End Time")],
-            )
-          ],
-        ),
-
         Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(Icons.skip_previous),
-            Icon(Icons.pause_circle_filled_rounded),
-            Icon(Icons.skip_next)
-          ],
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text("Current Time"), Text("End Time")],
         )
       ],
+    );
+  }
+}
+
+class Name extends StatelessWidget {
+  const Name({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [Text("Name"), Text("Artist")],
+    );
+  }
+}
+
+class Thumbnail extends StatelessWidget {
+  const Thumbnail({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.35,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.red,
     );
   }
 }
