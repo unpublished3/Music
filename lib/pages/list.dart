e96d69645e1f50fe5bc3e81849107468d8733def
unpublished3 <chapagainsakshyam@gmail.com>
2024-03-11 07:51:54 +0545

    Show name of track

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -28,7 +28,6 @@ class _ListUIState extends State<ListUI> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     setMetadata();
   }
@@ -37,44 +36,65 @@ class _ListUIState extends State<ListUI> {
     audioMetadata = await getMetadata(widget.file.path);
   }
 
+  String get trackName {
+    String? track = audioMetadata.trackName;
+    if (track != null) {
+      return formatName(basenameWithoutExtension(track));
+    }
+
+    return formatName(basenameWithoutExtension(widget.file.path));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 20, left: 20, top: 15, bottom: 0),
-      child: Container(
-        padding: EdgeInsets.all(12),
-        height: MediaQuery.of(context).size.height * 0.1,
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black, width: 2),
-            borderRadius: BorderRadius.circular(12)),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Container(
-                  width: MediaQuery.of(context).size.height * 0.07,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(color: Colors.black, width: 2)),
-                ),
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 6, vertical: 0),
-                  child: Column(
+    return FutureBuilder<void>(
+      future: setMetadata(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show a loading indicator while waiting for permission result
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          // Handle any errors
+          return Scaffold(body: Center(child: Text('Error occurred')));
+        } else {
+          // Permission granted or denied
+          return Padding(
+            padding:
+                const EdgeInsets.only(right: 20, left: 20, top: 15, bottom: 0),
+            child: Container(
+              padding: EdgeInsets.all(12),
+              height: MediaQuery.of(context).size.height * 0.1,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
                     children: [
-                      Text(formatName(basename(widget.file.path))),
-                      Text("Artist")
+                      Container(
+                        width: MediaQuery.of(context).size.height * 0.07,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(color: Colors.black, width: 2)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 6, vertical: 0),
+                        child: Column(
+                          children: [Text(trackName), Text("Artist")],
+                        ),
+                      )
                     ],
                   ),
-                )
-              ],
+                  Text("Length")
+                ],
+              ),
             ),
-            Text("Length")
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
