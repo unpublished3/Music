4aca517465c17f1b27927c76c39a923e6eeb43dc
unpublished3 <chapagainsakshyam@gmail.com>
2024-03-12 12:37:02 +0545

    Refactor list

--- lib/pages/list.dart
+++ lib/pages/list.dart
@@ -89,45 +89,79 @@ class _ListUIState extends State<ListUI> {
           return Scaffold(body: Center(child: Text('Error occurred')));
         } else {
           // Permission granted or denied
-          return Padding(
-            padding:
-                const EdgeInsets.only(right: 20, left: 20, top: 15, bottom: 0),
-            child: Container(
-              padding: EdgeInsets.all(12),
-              height: MediaQuery.of(context).size.height * 0.1,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.black, width: 2),
-                  borderRadius: BorderRadius.circular(12)),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.height * 0.07,
-                        decoration: BoxDecoration(
-                            image: DecorationImage(
-                                image: albumArt.image, fit: BoxFit.contain),
-                            borderRadius: BorderRadius.circular(10),
-                            border: Border.all(color: Colors.black, width: 2)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 6, vertical: 0),
-                        child: Column(
-                          children: [Text(trackName), Text(artistName)],
-                        ),
-                      )
-                    ],
-                  ),
-                  Text(trackDuration)
-                ],
-              ),
-            ),
-          );
+          return ListElement(albumArt: albumArt, trackName: trackName, artistName: artistName, trackDuration: trackDuration);
         }
       },
+    );
+  }
+}
+
+class ListElement extends StatelessWidget {
+  const ListElement({
+    super.key,
+    required this.albumArt,
+    required this.trackName,
+    required this.artistName,
+    required this.trackDuration,
+  });
+
+  final Image albumArt;
+  final String trackName;
+  final String artistName;
+  final String trackDuration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          const EdgeInsets.only(right: 20, left: 20, top: 15, bottom: 0),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        height: MediaQuery.of(context).size.height * 0.1,
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.black, width: 2),
+            borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                AlbumArt(albumArt: albumArt),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 6, vertical: 0),
+                  child: Column(
+                    children: [Text(trackName), Text(artistName)],
+                  ),
+                )
+              ],
+            ),
+            Text(trackDuration)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AlbumArt extends StatelessWidget {
+  const AlbumArt({
+    super.key,
+    required this.albumArt,
+  });
+
+  final Image albumArt;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.height * 0.07,
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: albumArt.image, fit: BoxFit.contain),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black, width: 2)),
     );
   }
 }
