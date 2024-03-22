362bff4484067ad0f802838ec8fd605f883a523f
unpublished3 <chapagainsakshyam@gmail.com>
2024-03-22 08:09:43 +0545

    Create player position provider

---
+++ lib/providers/player_position_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PlayerPositionProvider extends ChangeNotifier {
+    Duration _current = Duration.zero;
+    double _percentageComplete = 0;
+
+
+    Duration get current => _current;
+    double get percentageComplete => _percentageComplete;
+
+    void changePosition(Duration newPostion, Duration duration) {
+      _current = newPostion;
+      _percentageComplete = _current.inSeconds / duration.inSeconds;
+    }
+}
