1bd9d89347ba94f9953b2bee93f2721a90eb3026
unpublished3 <chapagainsakshyam@gmail.com>
2024-05-02 07:52:40 +0545

    Store current music in local storage

--- lib/providers/playlist_provider.dart
+++ lib/providers/playlist_provider.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:music/providers/player_provider.dart';
 import 'package:music/providers/player_status_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class PlaylistProvider extends ChangeNotifier {
   final List<File> _playlist = [];
@@ -36,12 +37,19 @@ class PlaylistProvider extends ChangeNotifier {
   }
 
   void setCurrent(context, String path) {
-    final playerStatusProvider = Provider.of<PlayerStatusProvider>(context, listen: false);
+    final playerStatusProvider =
+        Provider.of<PlayerStatusProvider>(context, listen: false);
 
     _current = path;
     playerStatusProvider.set(context, path);
+    _storeCurrent(path);
 
     notifyListeners();
+  }
+
+  void _storeCurrent(String path) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("storedPath", path);
   }
 
   String get current => _current;
