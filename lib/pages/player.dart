de8573582a6fb9b5bf35a2d70762fabded46ac94
unpublished3 <chapagainsakshyam@gmail.com>
2024-05-03 13:11:15 +0545

    Change border

--- lib/pages/player.dart
+++ lib/pages/player.dart
@@ -123,6 +123,7 @@ class _PlayerUIState extends State<PlayerUI> {
   @override
   Widget build(BuildContext context) {
     bool mode = Provider.of<PlaylistProvider>(context).mode;
+    bool isDark = MediaQuery.of(context).platformBrightness == Brightness.dark;
 
     return PopScope(
       canPop: false,
